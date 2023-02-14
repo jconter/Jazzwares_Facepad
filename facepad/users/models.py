0b40a94f22cbb0b719497c24f965a41f5f639db8
Joshua Conter <jconter3@gmail.com>
2023-02-13 23:03:42 -0500

    updated stock user model

--- facepad/users/models.py
+++ facepad/users/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    USER_TYPE_CHOICES = [
+        ('regular', 'Regular'),
+        ('admin', 'Admin')
+    ]
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField()
+    friends = models.ManyToManyField('self', blank=True)
+    user_type = models.CharField(
+        max_length=7,
+        choices=USER_TYPE_CHOICES,
+        default='regular',
+    )
